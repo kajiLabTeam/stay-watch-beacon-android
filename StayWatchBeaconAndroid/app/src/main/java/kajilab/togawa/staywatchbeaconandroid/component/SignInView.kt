993c03ef0e04232dee8d9baafbfb8be39e08ef66
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-08-05 22:26:09 +0900

    chore: サインインページのViewファイルのpackageを正しく

--- StayWatchBeaconAndroid/app/src/main/java/kajilab/togawa/staywatchbeaconandroid/component/SignInView.kt
+++ StayWatchBeaconAndroid/app/src/main/java/kajilab/togawa/staywatchbeaconandroid/component/SignInView.kt
@@ -1,4 +1,4 @@
-package com.example.staywatchbeaconandroid.component
+package kajilab.togawa.staywatchbeaconandroid.component
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
