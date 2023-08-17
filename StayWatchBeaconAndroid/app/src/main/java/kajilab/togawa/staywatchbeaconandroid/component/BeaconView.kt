fb2980e3e6c934b130dda847a738b8accc783a0f
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-08-17 11:15:01 +0900

    feat: VIew部分の完成

--- StayWatchBeaconAndroid/app/src/main/java/kajilab/togawa/staywatchbeaconandroid/component/BeaconView.kt
+++ StayWatchBeaconAndroid/app/src/main/java/kajilab/togawa/staywatchbeaconandroid/component/BeaconView.kt
@@ -2,6 +2,7 @@ package kajilab.togawa.staywatchbeaconandroid.component
 
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,11 +10,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -22,10 +28,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kajilab.togawa.staywatchbeaconandroid.R
 import kajilab.togawa.staywatchbeaconandroid.ui.theme.StayWatchBeaconAndroidTheme
 
 @Composable
@@ -42,6 +51,9 @@ fun BeaconView (modifier: Modifier = Modifier) {
     }
     val latestSyncTime = remember {
         mutableStateOf("2023/02/01 11:53")
+    }
+    val isAdvertising = remember {
+        mutableStateOf(false)
     }
 
 
@@ -97,18 +109,64 @@ fun BeaconView (modifier: Modifier = Modifier) {
                 .background(Color.Gray.copy(alpha = 0.15f)),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            // 発信中・停止中の四角
+            AdvertiseStatusPanel(
+                textStr = if(isAdvertising.value) "発信中" else "停止中",
+                panelColor = if(isAdvertising.value) Color(0xFF007AFF) else Color(0xFFFF3B30),
+                textColor = Color.White
+            )
+            // ユーザ名や同期ボタン、同期時刻
             Text(
-                text = userName.value
+                text = userName.value,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(bottom = 15.dp)
             )
             Text(
-                text = uuid.value
+                text = uuid.value,
+                fontSize = 12.sp,
+                modifier = Modifier
+                    .padding(bottom = 25.dp)
+            )
+//            Icon(
+//                painter = rememberVectorPainter(image = Icons.Default.Star),
+//                contentDescription = null,
+//            )
+            Image(
+                painter = painterResource(R.drawable.forward_circle),
+                contentDescription = null,
             )
             Text(
-                text = "最新の同期：" + latestSyncTime.value
+                text = "最新の同期：" + latestSyncTime.value,
+                color = Color.Gray,
+                fontSize = 12.sp,
+                modifier = Modifier
+                    .padding(bottom = 15.dp)
             )
         }
 
-
+        // 発信開始停止ボタン
+        if(isAdvertising.value){
+            Button(
+                onClick = {Log.d("Button", "発信を停止")},
+                colors = ButtonDefaults.buttonColors(Color.Transparent)
+            ) {
+                Text(
+                    text="発信を停止する",
+                    color = Color.Gray
+                )
+            }
+        } else {
+            Button(
+                onClick = {Log.d("Button", "発信を開始")},
+                colors = ButtonDefaults.buttonColors(Color.Transparent)
+            ) {
+                Text(
+                    text="発信を開始する",
+                    color = Color.Gray
+                )
+            }
+        }
 
     }
 }
